96bd7a620078af0744e1c07a2ff917ca92474e20
Aryo brokolly <98819851+aryobrokolly@users.noreply.github.com>
2024-06-07 21:14:52 +0700

    Update lite_watchdog.sh

--- log/lite_watchdog.sh
+++ log/lite_watchdog.sh
@@ -77,7 +77,7 @@ if [ $CNT -ge $4 ]; then
 			date +"%A %T %d-%m-%Y Status: OFFLINE > Action: Reboot" >> $LOG_FILE && sleep 5
 
 			logger -t LITE-WATCHDOG "Reboot"
-			mbit
+			python3 /usr/bin/orbit.py
 			;;
 		"wan")
 			[ -e /etc/lite_watchdog.user ] && env -i ACTION="wan" /bin/sh /etc/lite_watchdog.user
